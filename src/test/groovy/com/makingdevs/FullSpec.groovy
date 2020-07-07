8e637912cd0ac1dba74910a6b0946c9cea42ca8e
omar <omaraparicio07@gmail.com>
2020-07-06 19:39:18 -0500

    Add test using cycle life spock test

--- src/test/groovy/com/makingdevs/FullSpec.groovy
+++ src/test/groovy/com/makingdevs/FullSpec.groovy
@@ -6,13 +6,21 @@ class FullSpec extends Specification {
 
   def "Mostrar el ciclo de ejecución de spock" () {
     setup: "Inicializa los recursos para la especificación"
+      println "Inicializando ..."
+      String s = "Hola Mundo"
     and: "Más elementos de inicio"
-    given: "Dado un elemento al inicio"
-    and: "y algo mas al inicio"
-    when: "Ejecutas una accioón"
+      println "Inicializando algo mas"
+    //given: "Dado un elemento al inicio (es igual al seup pero solo se puede usar uno a la vez)"
+    //and: "y algo mas al inicio"
+    when: "Ejecutas una acción"
+      s = s.toUpperCase()
     and: "Ejecutas otra otra opcion"
-    expect: " Esperas un resultado"
+      int size = s.size()
+    then: " Esperas un resultado"
+      size == 10
+      s == "HOLA MUNDO"
     cleanup: "Limpias y reinicias los recuersos"
+      println "limpiando"
     where: "Tienes un conjunto de datos donde aplica para los casos que describas"
   }
 }
