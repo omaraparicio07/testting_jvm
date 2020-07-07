f9e0f76436ad9dc7249f41677ecaa1eb02e964e8
omar <omaraparicio07@gmail.com>
2020-07-06 19:27:12 -0500

    Add test using three steps 'given, when, then'

--- src/test/groovy/com/makingdevs/FirstSpec.groovy
+++ src/test/groovy/com/makingdevs/FirstSpec.groovy
@@ -7,4 +7,13 @@ class FirstSpec extends Specification {
     expect:
       1 == 1
   }
+
+  def "Agregar un nuevo objeto a un Set" () {
+    given:
+      def bag = [2,3,4,5] as Set
+    when:
+      bag << 1
+    then:
+    bag.size() == 5
+  }
 }
